7034db94a167623fd111f364409d5998135e5c77
doorbash <milad.doorbash@gmail.com>
2021-12-22 13:35:59 +0330

    fix DatabaseHelper cannot be cast to java.lang.AutoCloseable

--- library/src/main/java/backend/services/notifications/NotificationActivity.kt
+++ library/src/main/java/backend/services/notifications/NotificationActivity.kt
@@ -49,10 +49,11 @@ class NotificationActivity : Activity() {
                     javaClass.simpleName,
                     "error while sending click event for notification $id: ${e.message}"
                 )
-                DatabaseHelper(context).use {
-                    if (it.insertNotification(NotificationDB(id))) {
+                DatabaseHelper(context).apply {
+                    if (insertNotification(NotificationDB(id))) {
                         Log.d(javaClass.simpleName, "inserted notification $id into database")
                     }
+                    close()
                 }
             },
         )
